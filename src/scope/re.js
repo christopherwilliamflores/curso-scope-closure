11300bb07ab3967fa6d5a8b9d829c5b47e60c2e1
christopherwilliamflores <christopher.flores.ri@gmail.com>
2022-11-16 14:32:10 -0500

    changes

---
+++ src/scope/re.js
@@ -0,0 +1,33 @@
+var firstName; // Undefined
+firstName = 'Oscar';
+console.log(firstName);
+
+var lastName = "David"; // declarar / asignar
+lastName = 'Ana'; // reasignar
+console.log(lastName); 
+
+var secondName = 'David'; // declarando / asignando
+var secondName = 'Ana'; // reasignado
+console.log(secondName);
+
+// Let
+let fruit = 'Apple' // declara y asignar
+fruit = 'Kiwi'; // reasignar 
+console.log(fruit);
+
+let fruit = 'Banana'; //❌ NO SE PUEDE REDECLARAR UNA VARIABLE
+console.log(fruit);
+//--------------------------------------------
+
+// const
+const animal = 'dog'; // declara y asigna
+// animal = 'cat'; // reasignado ❌
+const animal = 'Snake'; //❌
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("🚗");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
