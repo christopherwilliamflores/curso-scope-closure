c06daae6dacbe8b75524a50f473103edab9f13ba
christopherwilliamflores <christopher.flores.ri@gmail.com>
2022-11-16 10:53:35 -0500

    global

---
+++ src/scope/global.js
@@ -0,0 +1,26 @@
+//variables
+
+var a;//declarando
+var b = 'b';// declaramos / asignamos
+b = 'bb';//reasignacion
+var a = 'aa' //redeclaracion
+
+
+//Glonal Scope
+var fruit = 'Apple';//global
+
+function bestFruit(){
+    console.log(fruit);
+}
+
+bestFruit();
+
+
+
+function countries(){
+    country = 'Colombia';// global ❌
+    console.log(country);
+}
+
+countries();
+console.log(country);
